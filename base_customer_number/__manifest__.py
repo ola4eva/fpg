b53a6730b10b55eba6686d1d687407af93540c27
obabawale <padinality@yahoo.com>
2022-11-06 17:42:22 +0000

    Add constraint to prevent duplication of customer number

---
+++ base_customer_number/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Base Customer Number",
+
+    'summary': """
+        Customer""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base', 'account'],
+
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+}
---
+++ base_customer_number/models/res_partner.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+
+
+class base_customer_number(models.Model):
+    _inherit = "res.partner"
+
+    customer_number = fields.Char("Customer Number")
+
+    @api.constrains("customer_number")
+    def _constrains_customer_number(self):
+        Partner = self.env["res.partner"]
+        partners_with_current_customer_number = (
+            Partner.search([("customer_number", "=", self.customer_number)]) - self
+        )
+        if partners_with_current_customer_number and self.customer_number:
+            raise UserError("The customer must be strictly unique!")
