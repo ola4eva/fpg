0695a295d2bc28350f49c407aa462139be76fd99
obabawale <padinality@yahoo.com>
2023-01-19 06:24:19 +0100

    [IMP] fpg_hr_expense: add module to manage notifications to employee's manage once an expense report is submitted

---
+++ fpg_hr_expense/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "HR Expense Mail Notifications",
+
+    'summary': """
+        Add email notifications to employee manager when an expense report is submitted""",
+
+    'description': """
+        Add email notifications to employee manager when an expense report is submitted. In the body of the email, add a link back to the record on odoo.
+    """,
+
+    'author': "Olalekan Babawale",
+    'website': "http://obabawale.github.io",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'views/views.xml',
+    ],
+}
